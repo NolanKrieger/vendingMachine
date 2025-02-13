d9a911473ddd882a18cea53faac71e5c37a9f69c
MarkP05 <mvpfister5@gmail.com>
2025-02-12 19:36:04 -0500

    Added basic switch case for framework

--- vending.java
+++ vending.java
@@ -1,5 +1,12 @@
 public class vending{
     public static void main(String[] args){
-        
+        int machineNumber = 1;
+
+        switch (machineNumber) {
+            case 1:
+                break;
+            default:
+                break;
+        }
     }
 }

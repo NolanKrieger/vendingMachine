d493542b4bf50b74a8840412b5fda6b1c8dd77f5
MarkP05 <mvpfister5@gmail.com>
2025-02-19 14:22:38 -0500

    Added more options, changed option identifiers, and adapted code to work with that. Also added some design.

--- vending.java
+++ vending.java
@@ -1,29 +1,55 @@
 import java.util.Scanner;
 
 public class vending {
-    public static double purchase(double machineNum, double price, double balance) {
+    public static double purchase(String machineNum, double price, double balance) {
         Scanner scan = new Scanner(System.in);
 
         double change = 0;
 
         // Added for better immersion, will take method inputs and provide a corresponding message so the user feels serviced.
-        if (machineNum == 1 && balance >= 2.49) {
+        if (machineNum.equals("1a")) {
             System.out.println("That will be $" + price);
-        } else if (machineNum == 2 && balance >= 2.49) {
+        } else if (machineNum.equals("2a")) {
             System.out.println("That will be $" + price);
-        } else if (machineNum == 3 && balance >= 1.49) {
+        } else if (machineNum.equals("3a")) {
             System.out.println("That will be $" + price);
-        } else if (machineNum == 4 && balance >= 1.99) {
+        } else if (machineNum.equals("4a")) {
             System.out.println("That will be $" + price);
-        } else if (machineNum == 5 && balance >= 2.49) {
+        } else if (machineNum.equals("5a")) {
             System.out.println("That will be $" + price);
-        } else if (machineNum == 6 && balance >= 2.99) {
+        } else if (machineNum.equals("6a")) {
             System.out.println("That will be $" + price);
-        } else if (machineNum == 7 && balance >= 2.99) {
+        } else if (machineNum.equals("7a")) {
             System.out.println("That will be $" + price);
-        } else if (machineNum == 8 && balance >= 2.99) {
+        } else if (machineNum.equals("8a")) {
             System.out.println("That will be $" + price);
-        } else if (machineNum == 9 && balance >= 2.99) {
+        } else if (machineNum.equals("9a")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("1b")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("2b")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("3b")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("4b")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("5b")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("6b")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("7b")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("8b")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("9b")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("1c")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("2c")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("3c")) {
+            System.out.println("That will be $" + price);
+        } else if (machineNum.equals("4c")) {
             System.out.println("That will be $" + price);
         }
 
@@ -75,16 +101,31 @@ public class vending {
         boolean firstRun = true;
         System.out.println("Welcome to the vending machine. Please enter money into the machine. Only $1 bills are allowed.\n\nHow many would you like to enter?");
 
-        System.out.println("1: Snickers: $2.49");
-        System.out.println("2: Milkyway: $2.49");
-        System.out.println("3: Starburst: $1.49");
-        System.out.println("4: Water: $1.99");
-        System.out.println("5: Gatorade: $2.49");
-        System.out.println("6: Lemonade: $2.99");
-        System.out.println("7: Coke: $2.99");
-        System.out.println("8: Pepsi: $2.99");
-        System.out.println("9: Dr Pepper: $2.99");
-        System.out.println("To exit, enter -1.");
+        System.out.println("===== REFRIDGERATED SECTION =====");
+        System.out.println("|| 1a - Coke             $2.99 ||");
+        System.out.println("|| 2a - Sprite           $2.99 ||");
+        System.out.println("|| 3a - 7-Up             $2.99 ||");
+        System.out.println("|| 4a - Pepsi            $2.99 ||");
+        System.out.println("|| 5a - Dr. Pepper       $2.99 ||");
+        System.out.println("|| 6a - Gatorade         $2.99 ||");
+        System.out.println("|| 7a - Lemonade         $2.99 ||");
+        System.out.println("|| 8a - Water            $1.99 ||");
+        System.out.println("|| 9a - Mountain Dew     $2.99 ||");
+        System.out.println("|| 1b - Sparkling Water  $2.99 ||");
+        System.out.println("========= SNACK SECTION =========");
+        System.out.println("|| 2b - Snickers         $2.49 ||");
+        System.out.println("|| 3b - Milkyway         $2.49 ||");
+        System.out.println("|| 4b - Starburst        $1.49 ||");
+        System.out.println("|| 5b - Cookies          $2.49 ||");
+        System.out.println("|| 6b - Pringles         $2.49 ||");
+        System.out.println("|| 7b - Chocolate        $1.99 ||");
+        System.out.println("|| 8b - Potato Chips     $1.49 ||");
+        System.out.println("|| 9b - Doritos          $2.49 ||");
+        System.out.println("|| 1c - Granola Bar      $2.49 ||");
+        System.out.println("|| 2c - M&M's            $1.49 ||");
+        System.out.println("|| 3c - Cheetos          $1.99 ||");
+        System.out.println("|| 4c - Pretzels         $1.49 ||");
+        System.out.println("======= To exit, enter -1 =======");
 
         while (hasMoney) {
             if (venScan.hasNextInt()) { // Check if the next input is a valid integer
@@ -114,65 +155,119 @@ public class vending {
 
             double price = 0;
 
-            int snickers = 1;
-            int milkyway = 2;
-            int starburst = 3;
-            int water = 4;
-            int gatorade = 5;
-            int lemonade = 6;
-            int coke = 7;
-            int pepsi = 8;
-            int drPepper = 9;
-
-            double price1 = 2.49;
-            double price2 = 2.49;
-            double price3 = 1.49;
-            double price4 = 1.99;
-            double price5 = 2.49;
+            double price1 = 2.99;
+            double price2 = 2.99;
+            double price3 = 2.99;
+            double price4 = 2.99;
+            double price5 = 2.99;
             double price6 = 2.99;
             double price7 = 2.99;
             double price8 = 2.99;
             double price9 = 2.99;
+            double price10 = 2.49;
+            double price11 = 2.49;
+            double price12 = 2.49;
+            double price13 = 1.49;
+            double price14 = 2.49;
+            double price15 = 2.49;
+            double price16 = 1.99;
+            double price17 = 2.49;
+            double price18 = 2.49;
+            double price19 = 2.49;
+            double price20 = 1.49;
+            double price21 = 1.99;
+            double price22 = 1.49;
 
             System.out.println("Please enter a number into the machine.");
-            int machineNumber = venScan.nextInt();
-            venScan.nextLine();
+            String machineNumber = venScan.nextLine();
 
             switch (machineNumber) {
-                case 1:
-                    System.out.println("Snickers selected.");
+                case "1a":
+                    System.out.println("Coke selected.");
                     price = price1;
                     break;
-                case 2:
-                    System.out.println("Milky Way selected.");
+                case "2a":
+                    System.out.println("Sprite selected.");
                     price = price2;
                     break;
-                case 3:
-                    System.out.println("Starburst selected.");
+                case "3a":
+                    System.out.println("7-Up selected.");
                     price = price3;
                     break;
-                case 4:
-                    System.out.println("Water selected.");
+                case "4a":
+                    System.out.println("Pepsi selected.");
                     price = price4;
                     break;
-                case 5:
-                    System.out.println("Gatorade selected.");
+                case "5a":
+                    System.out.println("Dr. Pepper selected.");
                     price = price5;
                     break;
-                case 6:
-                    System.out.println("Lemonade selected.");
+                case "6a":
+                    System.out.println("Gatorade selected.");
                     price = price6;
                     break;
-                case 7:
-                    System.out.println("Coke selected.");
+                case "7a":
+                    System.out.println("Lemonade selected.");
                     price = price7;
                     break;
-                case 8:
-                    System.out.println("Pepsi selected.");
+                case "8a":
+                    System.out.println("Water selected.");
                     price = price8;
                     break;
-                case 9:
-                    System.out.println("Dr Pepper selected.");
+                case "9a":
+                    System.out.println("Mountain Dew selected.");
+                    price = price9;
+                    break;
+                case "1b":
+                    System.out.println("Sparkling Water selected.");
+                    price = price9;
+                    break;
+                case "2b":
+                    System.out.println("Snickers selected.");
+                    price = price9;
+                    break;
+                case "3b":
+                    System.out.println("Milky Way selected.");
+                    price = price9;
+                    break;
+                case "4b":
+                    System.out.println("Starburst selected.");
+                    price = price9;
+                    break;
+                case "5b":
+                    System.out.println("Cookies selected.");
+                    price = price9;
+                    break;
+                case "6b":
+                    System.out.println("Pringles selected.");
+                    price = price9;
+                    break;
+                case "7b":
+                    System.out.println("Chocolate selected.");
+                    price = price9;
+                    break;
+                case "8b":
+                    System.out.println("Potato Chips selected.");
+                    price = price9;
+                    break;
+                case "9b":
+                    System.out.println("Doritos selected.");
+                    price = price9;
+                    break;
+                case "1c":
+                    System.out.println("Granola Bar selected.");
+                    price = price9;
+                    break;
+                case "2c":
+                    System.out.println("M&M's selected.");
+                    price = price9;
+                    break;
+                case "3c":
+                    System.out.println("Cheetos selected.");
+                    price = price9;
+                    break;
+                case "4c":
+                    System.out.println("Pretzels selected.");
                     price = price9;
                     break;
                 default:

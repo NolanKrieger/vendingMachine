e65c4ddb26150a08c3b2b9883cc2b6630407b8ce
NolanKrieger <nolankrieger9@gmail.com>
2025-02-18 19:14:43 -0500

    added a purchase method

--- vending.java
+++ vending.java
@@ -1,6 +1,33 @@
 import java.util.Scanner;
 
 public class vending{
+    public static double purchase(double price){
+
+        Scanner venScan = new Scanner(System.in);
+
+        System.out.println("That will be $" + price);
+        System.out.println("Please enter how much you are going to put in the machine.");
+        int amount = venScan.nextInt();
+        venScan.nextLine();
+        double change = amount - price;
+
+        System.out.println("Your change is $" + change);
+
+        
+
+
+        System.out.println("type continue to confirm purchase or type stop to cancel");
+        String choice = venScan.nextLine();
+        venScan.close();
+        
+        if(choice.equals("continue")){
+            return change;
+        }else{
+            return amount;
+        }
+
+        
+    }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int snickers = 1;
@@ -13,15 +40,15 @@ public class vending{
         int pepsi = 8;
         int drPepper = 9;
 
-        System.out.println("Snickers: $2.49");
-        System.out.println("Milkyway: $2.49");
-        System.out.println("Starburst: $1.49");
-        System.out.println("Water: $1.99");
-        System.out.println("Gatorade: $2.49");
-        System.out.println("Lemonade: $2.99");
-        System.out.println("Coke: $2.99");
-        System.out.println("Pepsei: $2.99");
-        System.out.println("Dr Pepper: $2.99");
+        System.out.println("1: Snickers: $2.49");
+        System.out.println("2: Milkyway: $2.49");
+        System.out.println("3: Starburst: $1.49");
+        System.out.println("4: Water: $1.99");
+        System.out.println("5: Gatorade: $2.49");
+        System.out.println("6: Lemonade: $2.99");
+        System.out.println("7: Coke: $2.99");
+        System.out.println("8: Pepsei: $2.99");
+        System.out.println("9: Dr Pepper: $2.99");
 
         System.out.println("Please enter a number into the machine.");
         int machineNumber = scan.nextInt();
@@ -29,6 +56,8 @@ public class vending{
         switch (machineNumber) {
             case 1:
                 System.out.println("Snickers selected.");
+                
+                
                 break;
             case 2:
                 System.out.println("Milky Way selected.");
